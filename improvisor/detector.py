1d53daff001aa174c214e87c1dd8db1d7bf81725
Ajit Nath <nath.ajit01@gmail.com>
2017-10-02 20:26:27 +0800

    linter config

--- improvisor/detector.py
+++ improvisor/detector.py
@@ -1,5 +1,5 @@
 """
-    Plays the song back to the user. This module uses fast fourier transform. 
+    Plays the song back to the user. This module uses fast fourier transform.
     A fast fourier transform converts an amplitude response to a frequency
     response. The required frequency can then then be selected from the response
     To know more, check out the wikipedia entries
@@ -20,7 +20,7 @@ class Detector(object):
         self.frames_per_fft = DETECTION_CONFIG['FRAMES_PER_FFT']
         self.note_min = DETECTION_CONFIG['NOTE_MIN']
         self.note_max = DETECTION_CONFIG['NOTE_MAX']
-        
+
         self.samples_per_fft = self.frame_size * self.frames_per_fft
         self.freq_step = self.sampling_rate / self.samples_per_fft
 
@@ -33,11 +33,11 @@ class Detector(object):
         frames_per_fft = self.frames_per_fft
         samples_per_fft = self.samples_per_fft
         freq_step = self.freq_step
-        
+
         imin = max(0, int(np.floor(
-                self._midi_to_fftbin(
-                    self.note_min
-                )
+            self._midi_to_fftbin(
+                self.note_min
+            )
         )))
         imax = max(0, int(np.floor(
             self._midi_to_fftbin(
@@ -53,44 +53,50 @@ class Detector(object):
         )
         hanning_window = 0.5 * (
             1 - np.cos(
-                np.linspace(0, 2 * np.pi, samples_per_fft, False
-                )
+                np.linspace(0, 2 * np.pi, samples_per_fft, False)
             )
         )
 
         frames_processed = 0
         stream_buffer = np.zeros(samples_per_fft, dtype=np.float)
-        stream.start_stream()     
-        
+        stream.start_stream()
+
         print('Sampling at {}hz with maximum resolution of {}'.format(sampling_rate, freq_step))
         while stream.is_active():
             stream_buffer[:-frame_size] = stream_buffer[frame_size:]
             stream_buffer[-frame_size:] = np.fromstring(
-                stream.read(frame_size), 
+                stream.read(frame_size),
                 np.int16
             )
             fft = np.fft.rfft(stream_buffer * hanning_window)
-            # Get freq response in range
             freq = (np.abs(fft[imin:imax]).argmax() + imin) * freq_step
-            
+
             note = _freq_to_midi(freq)
             note_abs = int(round(note))
 
             if frames_processed >= frames_per_fft:
                 print('freq: {:7.2f} Hz note: {:>3s} {:+.2f}'.format(
-                    freq, note_abs, note - note_abs                    
+                    freq, note_abs, note - note_abs
                 ))
-    
-    def play(self):
+
+    def play(self, sound_data):
         """
             Plays audio data using a MIDI library
         """
-    
-    def _midi_to_fftbin(self, n):
+        sampling_rate = self.sampling_rate
+        stream = pyaudio.PyAudio().open(
+            format=pyaudio.paInt16,
+            channels=1,
+            rate=sampling_rate,
+            output=True,
+        )
+        stream.write(sound_data)
+
+    def _midi_to_fftbin(self, note):
         """
             Pass
         """
-        return _midi_to_freq(n) / self.freq_step 
+        return _midi_to_freq(note) / self.freq_step
 
 # Internals
 
@@ -102,6 +108,6 @@ def _freq_to_midi(freq):
 
 def _midi_to_freq(midi):
     """
-        midi to frequency 
+        midi to frequency
     """
     return 440 * 2.0 ** ((midi - 69) / 12)
