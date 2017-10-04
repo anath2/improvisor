a8caf9df261056f9a05b01558b1f6c5446ee7804
Ajit Nath <nath.ajit01@gmail.com>
2017-10-04 21:38:49 +0800

    Minor updates - variable names, time recording

--- improvisor/detector.py
+++ improvisor/detector.py
@@ -4,6 +4,7 @@
     response. The required frequency can then then be selected from the response
     To know more, check out the wikipedia entries
 """
+import time
 import numpy as np
 import pyaudio
 
@@ -14,23 +15,25 @@ class Detector(object):
         Detection module reads sound data using python audio library(pyAudio)
         configuration files are loaded from config module
     """
+
     def __init__(self):
         self.sampling_rate = DETECTION_CONFIG['SAMPLING_RATE']
-        self.frame_size = DETECTION_CONFIG['FRAME_SIZE']
-        self.frames_per_fft = DETECTION_CONFIG['FRAMES_PER_FFT']
+        self.chunk_size = DETECTION_CONFIG['FRAME_SIZE']
+        self.chunks_per_fft = DETECTION_CONFIG['FRAMES_PER_FFT']
         self.note_min = DETECTION_CONFIG['NOTE_MIN']
         self.note_max = DETECTION_CONFIG['NOTE_MAX']
 
-        self.samples_per_fft = self.frame_size * self.frames_per_fft
+        self.samples_per_fft = self.chunk_size * self.chunks_per_fft
         self.freq_step = self.sampling_rate / self.samples_per_fft
+        # The variable samples per fft is the unit in frequency domain
 
     def listen(self):
         """
             Read audio
         """
         sampling_rate = self.sampling_rate
-        frame_size = self.frame_size
-        frames_per_fft = self.frames_per_fft
+        chunk_size = self.chunk_size
+        chunks_per_fft = self.chunks_per_fft
         samples_per_fft = self.samples_per_fft
         freq_step = self.freq_step
 
@@ -49,7 +52,7 @@ class Detector(object):
             channels=1,
             rate=sampling_rate,
             input=True,
-            frames_per_buffer=frame_size
+            frames_per_buffer=chunk_size
         )
         hanning_window = 0.5 * (
             1 - np.cos(
@@ -57,15 +60,19 @@ class Detector(object):
             )
         )
 
+        recording_data = []
         frames_processed = 0
+        recording_time = 5.0 # Record for 5 seconds
         stream_buffer = np.zeros(samples_per_fft, dtype=np.float)
         stream.start_stream()
 
         print('Sampling at {}hz with maximum resolution of {}'.format(sampling_rate, freq_step))
+
+        start_time = time.time()
         while stream.is_active():
-            stream_buffer[:-frame_size] = stream_buffer[frame_size:]
-            stream_buffer[-frame_size:] = np.fromstring(
-                stream.read(frame_size),
+            stream_buffer[:-chunk_size] = stream_buffer[chunk_size:]
+            stream_buffer[-chunk_size:] = np.fromstring(
+                stream.read(chunk_size),
                 np.int16
             )
             fft = np.fft.rfft(stream_buffer * hanning_window)
@@ -75,11 +82,21 @@ class Detector(object):
             note = _freq_to_midi(freq)
             note_abs = int(round(note))
             frames_processed += 1
-            if frames_processed >= frames_per_fft:
+            if frames_processed >= chunks_per_fft:
                 if np.average(power) > 60:
                     print('freq: {:7.2f} power: {} Hz note: {} {:+.2f}'.format(
                         freq, power, note_abs, note - note_abs
                     ))
+                    recording_data.append((time.time() - start_time, note_abs,))
+            if time.time() - start_time > recording_time:
+                return recording_data
+
+    def record_processing(self, recording_data):
+        """
+            Convert the frequency and time data
+            into wave
+        """
+        pass
 
     def play(self, sound_data):
         """
