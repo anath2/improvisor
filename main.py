c63929527f00774b44d5370c9315566119c4718f
Ajit Nath <nath.ajit01@gmail.com>
2017-10-27 23:50:48 +0800

    Refactor

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 """
     Runner
 """
-from improvisor.detector import Detector
+from improvisor.sound_io import SoundDetector, SoundPlayer
 def main():
     """
         Detector runner
     """
-    det = Detector()
-    sound_data = det.listen()
-    det.play(sound_data)
+    # Record
+    detector = SoundDetector()
+    recorded_data = detector.listen()
+
+    # Playback
+    player = SoundPlayer()
+    player.play(recorded_data)
 
 if __name__ == '__main__':
     main()
