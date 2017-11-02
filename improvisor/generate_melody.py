927201ec5f30bf236468fdc9a4c28388e2f07ba2
Ajit Nath <nath.ajit01@gmail.com>
2017-11-03 00:17:56 +0800

    Adding tensorflow

---
+++ improvisor/generate_melody.py
@@ -0,0 +1,21 @@
+"""
+    Creates melody from user input
+"""
+import tensorflow as tf
+
+class MelodyModel(object):
+    """
+        Neural network for generating melody data
+    """
+
+    def generate_melody(self):
+        """
+            Generates a new melody
+        """
+        pass
+
+    def melody_likelyhood(self):
+        """
+            Likelyhood of the melody generated
+        """
+        pass
