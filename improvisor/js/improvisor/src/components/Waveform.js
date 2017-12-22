9ff832075670dfa6742ae257a882d8ff2ae72835
Ajit Nath <nath.ajit01@gmail.com>
2017-12-22 23:50:32 +0800

    Waveform [WIP]

--- improvisor/js/improvisor/src/components/Waveform.js
+++ improvisor/js/improvisor/src/components/Waveform.js
@@ -1,5 +1,12 @@
 import React, { Components } from 'react'
 
+// Bar component whose height is set by the wave
+const WaveBar = ({style}) => {
+    return (
+        <div className='wave-bar' style={style} />
+    )
+}
+
 const WaveForm = () => {
     return (
         <div className='waveform-wrapper'>
