c9a58d6438888df3be99971f767481ada7793fea
Ajit Nath <nath.ajit01@gmail.com>
2018-01-12 21:25:10 +0800

    Setting up flask server for displaying the homepage

--- improvisor/server/server.py
+++ improvisor/server/server.py
@@ -4,23 +4,22 @@
 import os
 from flask import Flask, render_template
 
+static_dir = os.path.join(
+    os.path.dirname(os.path.abspath(os.path.dirname(__file__))),
+    'client',
+    'dist'
+)
+
 template_dir = os.path.join(
     os.path.dirname(os.path.abspath(os.path.dirname(__file__))),
-    'static'
+    'client'
 )
-app = Flask(__name__, template_folder=template_dir)
+
+app = Flask(__name__, static_folder=static_dir, template_folder=template_dir)
+
 @app.route('/')
 def show_home():
-    """
-        Homepage
-    """
-
     return render_template('index.html')
 
-@app.route('/', methods=['POST'])
-def get_input():
-    """
-        Post sound data
-    """
-    pass
-    # Process the sound data
+if __name__ == '__main__':
+    app.run()
