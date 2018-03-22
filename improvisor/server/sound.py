cb4a262fb62664ca8f4a258683c7e28dc31ae385
Ajit Nath <nath.ajit01@gmail.com>
2018-03-22 23:56:34 +0800

    Setting aubio

--- improvisor/server/sound.py
+++ improvisor/server/sound.py
@@ -18,11 +18,12 @@ Module contents:
     animation
 """
 from abc import ABCMeta, abstractmethod
+from aubio import pitch
 
 class SoundInterface(ABCMeta):
 
     @abstractmethod
-    def listen(self):
+    def read_audio(self):
       '''
       Listen audio
       '''
@@ -36,7 +37,7 @@ class SoundInterface(ABCMeta):
       pass
 
     @abstractmethod
-    def write(self):
+    def write_audio(self):
       '''
       Write audio data to the file or audio output
       '''
@@ -44,9 +45,18 @@ class SoundInterface(ABCMeta):
 
 class Improvisor(SoundInterface):
   '''
-  Improvisor
+  Improvisor listen/read to audio from a microphone or a
+  file and play/write computer generated response
   '''
-  pass
+  def __init__(self):
+    tolerance = 0.8
+    downsample = 1
+    win_s = 4096 // downsample # fft size
+    hop_s = 1024  // downsample # hop size
+    pitch_o = pitch("yin", win_s, hop_s, RATE)
+    pitch_o.set_unit("midi")
+    pitch_o.set_tolerance(tolerance)
+
 
 
   # Private methods
@@ -341,3 +351,62 @@ class Improvisor(SoundInterface):
 #             duration = current_times[-1] - current_times[0]
 #             processed.append((current_note, delay, duration,))
 #         return processed
+# #! /usr/bin/env python
+# import pyaudio
+# import wave
+# import numpy as np
+# from aubio import pitch
+
+# CHUNK = 1024
+# FORMAT = pyaudio.paFloat32
+# CHANNELS = 1
+# RATE = 44100
+# RECORD_SECONDS = 5
+# WAVE_OUTPUT_FILENAME = "output.wav"
+
+# p = pyaudio.PyAudio()
+
+# stream = p.open(format=FORMAT,
+#                 channels=CHANNELS,
+#                 rate=RATE,
+#                 input=True,
+#                 frames_per_buffer=CHUNK)
+
+# print("* recording")
+
+# frames = []
+
+# # Pitch
+# tolerance = 0.8
+# downsample = 1
+# win_s = 4096 // downsample # fft size
+# hop_s = 1024  // downsample # hop size
+# pitch_o = pitch("yin", win_s, hop_s, RATE)
+# pitch_o.set_unit("midi")
+# pitch_o.set_tolerance(tolerance)
+
+
+# for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+#     buffer = stream.read(CHUNK)
+#     frames.append(buffer)
+
+#     signal = np.fromstring(buffer, dtype=np.float32)
+
+#     pitch = pitch_o(signal)[0]
+#     confidence = pitch_o.get_confidence()
+
+#     print("{} / {}".format(pitch,confidence))
+
+
+# print("* done recording")
+
+# stream.stop_stream()
+# stream.close()
+# p.terminate()
+
+# wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+# wf.setnchannels(CHANNELS)
+# wf.setsampwidth(p.get_sample_size(FORMAT))
+# wf.setframerate(RATE)
+# wf.writeframes(b''.join(frames))
+# wf.close()
